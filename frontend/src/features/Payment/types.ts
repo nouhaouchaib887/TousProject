e7f7c5b3ea85958ba7c9fa26e980c341928189d4
Nouha Ouchaib <nouhaouchaib887@gmail.com>
2026-06-25 20:35:38 +0100

    temporary

---
+++ frontend/src/features/Payment/types.ts
@@ -0,0 +1,84 @@
+
+export interface Product{
+    id?: string
+    label?:string
+    reference?:string
+    
+}
+
+
+export interface InvoiceItem{
+    id?:string;
+    local_id?:string
+    item?:Product;
+    unit?:string
+    quantity?:number
+    amount_type:string;
+    amount_ttc?:number
+    amount_ht?:number
+    vat_rate?:number
+    
+}
+export interface Transaction{
+    id?:string;
+    reference?:string;
+    local_id?:string;
+    amount:number;
+    payment_method?:string
+    payment_reference?:string
+    payment_date?:string
+    check_date?:string 
+    
+}
+export interface Partner{
+    id?:string
+    last_name?:string
+    first_name?:string
+    full_name?:string
+    phone_number?:string
+    cin?:string
+}
+export interface InvoiceRead {
+    id:string
+    reference:string
+}
+export interface PaymentTableRead {
+    id:string;
+    reference:string;
+    invoice: InvoiceRead
+    partner?:Partner
+    amount:number;
+    payment_method:string
+    payment_reference?:string
+    payment_date: string
+    check_date?:string 
+    status?:string
+
+}
+
+export interface InvoiceMetaData {
+    reference?:string
+    due_date:string;
+    invoice_date:string
+    partner?:Partner
+    invoice_type:string;
+    payment_method:string
+    expected_check_number?: string
+    expected_check_date?: string
+}
+export interface InvoiceCreate {
+    id?:string
+    invoice_metadata: InvoiceMetaData
+    invoice_items: InvoiceItemsDataSection
+    transactions: Transaction[]
+
+
+}
+
+export interface InvoiceItemsDataSection{
+    invoice_items?: InvoiceItem []
+    amount_type:string;
+    amount_ttc?:number
+    amount_ht?:number
+    total_vat?: number
+}
